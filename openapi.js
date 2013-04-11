e57ec0cf62894c3961fc1857ebc17007825511c6
Chris Wallis <chris@inybble.com>
2013-04-11 22:59:19 +0200

    Added beginnings of cdn.Job, Removed stringify code

--- openapi.js
+++ openapi.js
@@ -10,6 +10,12 @@ cardeliverynetwork.openapi.prototype = {
         call(this._apiUrl, this._apiKey, resource, "POST", onSuccess, onFail, false, jobs);
     },
 
+    createJob: function (job, onSuccess, onFail) {
+        this.createJobs([job], function (responseData) {
+            onSuccess(responseData[0]);
+        }, onFail);
+    },
+
     getJob: function (id, onSuccess, onFail) {
         var resource = "Jobs/" + id;
         call(this._apiUrl, this._apiKey, resource, "GET", onSuccess, onFail);
@@ -45,6 +51,31 @@ cardeliverynetwork.openapi.prototype = {
         call(this._apiUrl, this._apiKey, resource, "GET", onSuccess, onFail);
     }
 }
+cardeliverynetwork.Job = function () {
+    this.Notes = "",
+    this.CustomerReference = "",
+    this.Customer = {
+        AddressLines: "",
+        City: ""
+    }
+    this.Pickup = {
+        Destination: {
+            AddressLines: "",
+            City: "",
+        },
+        RequestedDate: null,
+        RequestedDateIsExact: false
+    },
+    this.Dropoff = {
+        Destination: {
+            AddressLines: "",
+            City: ""
+        },
+        RequestedDate: null,
+        RequestedDateIsExact: false
+    },
+    this.Vehicles = [{}]
+}
 
 function call(apiUrl, apiKey, resuorce, method, onSuccess, onFail, isUsingRemoteIds, data) {
     var callUrl = apiUrl + "/" + resuorce + "?ApiKey=" + apiKey + "&format=json";
@@ -53,7 +84,7 @@ function call(apiUrl, apiKey, resuorce, method, onSuccess, onFail, isUsingRemote
         contentType: 'application/json',
         type: method,
         url: callUrl,
-        data: stringify(data),
+        data: JSON.stringify(data),
         success: function (responseData) {
             onSuccess(responseData);
         },
@@ -61,93 +92,4 @@ function call(apiUrl, apiKey, resuorce, method, onSuccess, onFail, isUsingRemote
             onFail(errorData.status, errorData.statusText);
         }
     });
-}
-
-function stringify(value, replacer, space) {
-    var i;
-    gap = '';
-    indent = '';
-    if (typeof space === 'number') {
-        for (i = 0; i < space; i += 1) {
-            indent += ' ';
-        }
-    } else if (typeof space === 'string') {
-        indent = space;
-    }
-    rep = replacer;
-    if (replacer && typeof replacer !== 'function' &&
-        (typeof replacer !== 'object' ||
-        typeof replacer.length !== 'number')) {
-        throw new Error('JSON.stringify');
-    }
-    return str('', {
-        '': value
-    });
-};
-
-function str(key, holder) {
-    var i,
-        k,
-        v,
-        length,
-        mind = gap,
-        partial,
-        value = holder[key];
-    if (value && typeof value === 'object' &&
-        typeof value.toJSON === 'function') {
-        value = value.toJSON(key);
-    }
-    if (typeof rep === 'function') {
-        value = rep.call(holder, key, value);
-    }
-    switch (typeof value) {
-		case 'string':
-			return quote(value);
-		case 'number':
-			return isFinite(value) ? String(value) : 'null';
-		case 'boolean':
-		case 'null':
-			return String(value);
-		case 'object':
-			if (!value) {
-				return 'null';
-			}
-			gap += indent;
-			partial = [];
-			if (Object.prototype.toString.apply(value) === '[object Array]') {
-				length = value.length;
-				for (i = 0; i < length; i += 1) {
-					partial[i] = str(i, value) || 'null';
-				}
-				v = partial.length === 0 ? '[]' : gap ? '[\n' + gap + partial.join(',\n' + gap) + '\n' + mind + ']' : '[' + partial.join(',') + ']';
-				gap = mind;
-				return v;
-			}
-
-			if (rep && typeof rep === 'object') {
-				length = rep.length;
-				for (i = 0; i < length; i += 1) {
-					if (typeof rep[i] === 'string') {
-						k = rep[i];
-						v = str(k, value);
-						if (v) {
-							partial.push(quote(k) + (gap ? ': ' : ':') + v);
-						}
-					}
-				}
-			} else {
-				for (k in value) {
-					if (Object.prototype.hasOwnProperty.call(value, k)) {
-						v = str(k, value);
-						if (v) {
-							partial.push(quote(k) + (gap ? ': ' : ':') + v);
-						}
-					}
-				}
-			}
-
-			v = partial.length === 0 ? '{}' : gap ? '{\n' + gap + partial.join(',\n' + gap) + '\n' + mind + '}' : '{' + partial.join(',') + '}';
-			gap = mind;
-			return v;
-		}
 }
